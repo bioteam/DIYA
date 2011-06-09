ff1c632a3aa9e2ab0f88370aba62565b509c0290
Brian Osborne <briano@bioteam.net>
2011-06-09 10:06:35 -0400

    Make modules for MARC, move unused

---
+++ lib/diya/MARC/CTR.pm
@@ -0,0 +1,77 @@
+# $Id: template.pm 188 2008-08-12 15:22:09Z briano $
+#--------------------------------------------------------------------------
+# ©Copyright 2008
+#
+# This file is part of DIYA.
+#
+# DIYA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DIYA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the diya package.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------
+
+=head1 NAME
+
+
+=head1 SYNOPSIS
+
+Do not use this module directly. This module is used by diya.pm when it
+runs a pipeline.
+
+=head1 DESCRIPTION
+
+Template Perl module. A parser() method is required.
+
+=head1 AUTHOR
+
+Andrew Stewart, andrew.stewart@med.navy.mil
+
+=head1 CONTRIBUTORS
+
+Tim Read, timothy.read@med.navy.mil
+Brian Osborne, briano@bioteam.net
+
+=cut
+
+# add the new module name here
+package diya::;
+
+use strict;
+# simplest approach
+use base 'diya';
+
+# alternate approach, if a variable needs to be imported
+# use vars qw(@ISA);
+# use diya qw();
+# @ISA = qw(diya);
+
+
+=head2 parse
+
+ Name    : parse
+ Usage   : $parser->parse($diya)
+ Function: parse a program output file - every parser module must have a 
+           parse() method
+ Returns : 1 on success
+ Args    : a diya object
+ Example : 
+
+=cut
+
+sub parse {
+
+	1;
+}
+
+1;
+
+__END__
+
