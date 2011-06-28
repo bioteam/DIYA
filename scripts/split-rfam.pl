2129c2687dca4ebe351acd107f489d75627e5b1c
Brian O <briano@bioteam.net>
2011-06-28 12:23:54 -0500

    Add cmsearch

--- scripts/split-rfam.pl
+++ scripts/split-rfam.pl
@@ -4,22 +4,20 @@
 use strict;
 
 open MYIN,"Rfam.full";
-my $num = 0;
+my $text = '';
 
 while (my $line = <MYIN>) {
 
-    if ( $line =~ /^# STOCKHOLM/ ) {
-	 $num++;
-	 my $id = 'RF' . sprintf("%05d", $num);
-	 open MYALN,">>aln/$id.aln";
+    if ( $line =~ /^# STOCKHOLM/ && $text ) {
+	 my ($id) = $text =~ /(RF\d+)/;
+	 open MYALN,">aln/$id.aln";
+	 print MYALN $text;
+	 $text = '';
     }
 
-    print MYALN $line;
-
+    $text .= $line;
 }
 
 __END__
 
-    `/usr/local/share/infernal-1.0.1/bin/cmbuild $id.cm aln/$id.aln`;
-    `/usr/local/share/infernal-1.0.1/bin/cmcalibrate $id.cm`;
-    print "Done with $id.cm\n";
+
